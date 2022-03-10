85339d089b89a5d5c42ae112846e279d1b20bc1d
mohkh833 <55802148+mohkh833@users.noreply.github.com>
2022-03-10 08:17:10 +0200

    Update editprofile.js

--- client/client/src/pages/editprofile.js
+++ client/client/src/pages/editprofile.js
@@ -21,6 +21,7 @@ const Register = () => {
     const [success, setSuccess] = useState(false)
     let id = +localStorage.getItem('userId')
 
+    //submit edit
     const handleClick = async () => {
         let body = {email, name, password,imgUrl}
         try{
@@ -33,7 +34,7 @@ const Register = () => {
     }
 
     
-    console.log(id)
+    //fetch user data
     useEffect(()=> {
         let mounted = true
             const fetchUser = async() => {
@@ -51,83 +52,83 @@ const Register = () => {
         return ()=>(mounted=false)
     },[id])
 
-    
+    // edit profile code
     return (
         <React.Fragment>
         <NavBar/>
-        <Grid container justifyContent="center">
-            <Grid item>
-                <Typography  variant="h4">Edit Profile Page</Typography>
-                <Paper>
-                    <Box m={2} p={5} alignItems="center">
-                        <Box
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                        ></Box>
-                        <Box>
-                            <TextField
-                                label={data.email}
-                                type="text"
-                                style={{ width: "400px" }}
-                                onChange={(e)=> setEmail(e.target.value)}
-                            ></TextField>
+            <Grid container justifyContent="center">
+                    <Grid item>
+                        <Typography  variant="h4">Edit Profile Page</Typography>
+                        <Paper>
+                            <Box m={2} p={5} alignItems="center">
+                                <Box
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                                ></Box>
+                                <Box>
+                                    <TextField
+                                        label={data.email}
+                                        type="text"
+                                        style={{ width: "400px" }}
+                                        onChange={(e)=> setEmail(e.target.value)}
+                                    ></TextField>
+                                    </Box>
+                                    <br/>
+                                    <Box>
+                                    <TextField
+                                        label={data.name}
+                                        type="text"
+                                        style={{ width: "400px" }}
+                                        onChange={(e)=> setName(e.target.value)}
+                                    ></TextField>
+                                    </Box>
+                                    <br/>
+                                    <Box>
+                                    <TextField
+                                        label={data.imgUrl}
+                                        type="text"
+                                        style={{ width: "400px" }}
+                                        onChange={(e)=> setImgUrl(e.target.value)}
+                                    ></TextField>
+                                    </Box>
+                                    <br/>
+                                    <Box>
+                                    <TextField
+                                        label="password"
+                                        type="password"
+                                        style={{ width: "400px" }}
+                                        onChange={(e)=> setPassword(e.target.value)}
+                                    ></TextField>
+                                </Box>
+                                <Box m={3}>
+                                    <Button
+                                        variant="contained"
+                                        style={{ backgroundColor: "green", color: "floralwhite" }}
+                                        type="button"
+                                        onClick={handleClick}
+                                        >
+                                        Edit
+                                    </Button>
+                                </Box>
                             </Box>
-                            <br/>
-                            <Box>
-                            <TextField
-                                label={data.name}
-                                type="text"
-                                style={{ width: "400px" }}
-                                onChange={(e)=> setName(e.target.value)}
-                            ></TextField>
-                            </Box>
-                            <br/>
-                            <Box>
-                            <TextField
-                                label={data.imgUrl}
-                                type="text"
-                                style={{ width: "400px" }}
-                                onChange={(e)=> setImgUrl(e.target.value)}
-                            ></TextField>
-                            </Box>
-                            <br/>
-                            <Box>
-                            <TextField
-                                label="password"
-                                type="password"
-                                style={{ width: "400px" }}
-                                onChange={(e)=> setPassword(e.target.value)}
-                            ></TextField>
-                        </Box>
-                        <Box m={3}>
-                            <Button
-                                variant="contained"
-                                style={{ backgroundColor: "green", color: "floralwhite" }}
-                                type="button"
-                                onClick={handleClick}
-                                >
-                                Edit
-                            </Button>
-                        </Box>
-                    </Box>
-                </Paper>
-                {err && (
-                    <React.Fragment>
-                        <Stack sx={{ width: '100%' }} spacing={2}>
-                            <Alert severity="error">Your Email is maybe used or you didn't updated your data</Alert>
-                        </Stack>
-                    </React.Fragment> 
-                )}
-                {success && (
-                    <React.Fragment>
-                        <Stack sx={{ width: '100%' }} spacing={2}>
-                            <Alert severity="success">Post is updated successfully</Alert>
-                        </Stack>
-                </React.Fragment> 
-                )}
-            </Grid>
-            </Grid>
+                        </Paper>
+                        {err && (
+                            <React.Fragment>
+                                <Stack sx={{ width: '100%' }} spacing={2}>
+                                    <Alert severity="error">Your Email is maybe used or you didn't updated your data</Alert>
+                                </Stack>
+                            </React.Fragment> 
+                        )}
+                        {success && (
+                            <React.Fragment>
+                                <Stack sx={{ width: '100%' }} spacing={2}>
+                                    <Alert severity="success">Post is updated successfully</Alert>
+                                </Stack>
+                        </React.Fragment> 
+                        )}
+                    </Grid>
+                </Grid>
             </React.Fragment>
         )
 }
